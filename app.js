c9bcf33d171c0b7b90070ac6dcb7792e79c65c5e
firdausvrisca <firdaus.vrisca@gmail.com>
2019-11-21 10:45:14 +0700

    Create app.js
    Internet Of Things

---
+++ app.js
@@ -0,0 +1,58 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+// var bodyParser = require('body-parser');
+
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var postRouter = require('./routes/post');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+app.use('/post', postRouter);
+
+app.get('/html',function(req,res){
+  res.sendFile(path.join(__dirname+'/routes/socket.html'));
+  //__dirname : It will resolve to your project folder.
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next.status(404).send({
+    error: err.message
+  }),
+  res.status(422).send({
+    error: err.message
+  })
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  // res.locals.message = err.message;
+  // res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // // render the error page
+  // res.status(err.status || 500);
+  // res.render('error');
+  res.status(500).send({error: err.message});
+  console.log(err.message);
+});
+
+module.exports = app;
